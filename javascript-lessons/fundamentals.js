e3ec3d5ca93d64795d98bc2eb3993ab93d551c30
Andrey Gomes <andreygdsn@gmail.com>
2022-04-09 19:37:04 -0300

    fundamentals complete

--- javascript-lessons/fundamentals.js
+++ javascript-lessons/fundamentals.js
@@ -1,6 +1,21 @@
-let name = "John"
-let admin;
+function add7(num){
+    return num+7;
+}
 
-admin = name
+function multiply(num, times){
+    return num * times;
+}
 
-console.log(name)
+function capitalize(text){
+    text = text.toLowerCase()
+    return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+function lastLetter(text){
+    return text.charAt(text.length-1);
+}
+
+console.log(add7(10));
+console.log(multiply(22, 3));
+console.log(capitalize('arroz e FEIJÃO'));
+console.log(lastLetter('cookie'));
